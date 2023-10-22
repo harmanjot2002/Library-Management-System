dcf07faead54c6a4bb88c2cb88fa9b809ec0c096
harmanjot2002 <harmanjot0555.be21@chitkara.edu.in>
2023-10-22 12:48:13 +0530

    Display Records

--- BookStore.java
+++ BookStore.java
@@ -2,70 +2,82 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+
 public class BookStore implements ActionListener {
     private static JFrame f;
     private static ImageIcon imageIcon;
     private static JLabel imageLabel;
-   public BookStore(){
-        f= new JFrame("Book Store");
+
+    public BookStore() {
+        f = new JFrame("Book Store");
         f.setSize(600, 500);
         f.setLayout(null);
-        Dimension screenSize=Toolkit.getDefaultToolkit().getScreenSize();
-        f.setLocation((int)(screenSize.getWidth()-f.getWidth())/2,(int)(screenSize.getHeight()-f.getHeight())/2);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        f.setLocation((int) (screenSize.getWidth() - f.getWidth()) / 2, (int) (screenSize.getHeight() - f.getHeight()) / 2);
         f.setUndecorated(true);
         f.getRootPane().setBorder(BorderFactory.createLineBorder(new Color(102, 0, 51), 3));
-        
-        JLabel heading=new JLabel("Book Store");
-        heading.setBounds(170,50,400,50);
+
+        JLabel heading = new JLabel("Book Store");
+        heading.setBounds(170, 50, 400, 50);
         heading.setFont(new Font("Serif", Font.BOLD, 55));
-        heading.setForeground(new Color(102, 0, 51)); 
+        heading.setForeground(new Color(102, 0, 51));
         f.add(heading);
 
-        JButton addbtn=new JButton("Add");
+        JButton addbtn = new JButton("Add");
         addbtn.setForeground(Color.white);
-       addbtn.setBackground(Color.BLUE);
-        // addbtn.setBackground(new Color(63, 152, 76));
-        addbtn.setBounds(150,150,110,50);
+        addbtn.setBackground(Color.BLUE);
+        addbtn.setBounds(150, 180, 110, 50);
         f.add(addbtn);
         addbtn.addActionListener(this);
 
-        JButton deletebtn=new JButton("Delete");
-       // deletebtn.setBackground(Color.red);
-       deletebtn.setForeground(Color.white);
+        JButton deletebtn = new JButton("Delete");
+        deletebtn.setForeground(Color.white);
         deletebtn.setBackground(new Color(153, 0, 76));
-        deletebtn.setBounds(340,150,110,50);
+        deletebtn.setBounds(340, 180, 110, 50);
         f.add(deletebtn);
 
-        JButton updatebtn=new JButton("Update");
+        JButton updatebtn = new JButton("Update");
         updatebtn.setForeground(Color.white);
-       updatebtn.setBackground(new Color(0, 128, 0));
-        // updatebtn.setBackground(new Color(206, 49, 39));
-         updatebtn.setBounds(150,280,110,50);
+        updatebtn.setBackground(new Color(0, 128, 0));
+        updatebtn.setBounds(150, 280, 110, 50);
         f.add(updatebtn);
 
-        JButton exit =new JButton("Exit");
-        exit.setForeground(Color.white);
-         exit.setBackground(Color.RED);
-        //  exit.setBackground(new Color(226, 18, 93));
-        exit.setBounds(340,280,110,50);
-          f.add(exit);
+        JButton displaybtn = new JButton("Display");
+        displaybtn.setBackground(Color.pink);
+        displaybtn.setForeground(Color.white);
+        displaybtn.setBounds(340, 280, 110, 50);
+        f.add(displaybtn);
 
-            JButton login=new JButton("Back to Login");
-         //exit.setBackground(Color.blue);
-         login.setForeground(Color.white);
-         login.setBackground(new Color(255, 128, 0));
-        login.setBounds(240,380,130,60);
-          f.add(login);
-          exit.addActionListener(e -> System.exit(0));
-          JPanel panel2=new JPanel();
-        panel2.setBounds(0,0,800, 600);
+        JButton exit = new JButton("Exit");
+        exit.setForeground(Color.white);
+        exit.setBackground(Color.RED);
+        exit.setBounds(150, 380, 110, 50);
+        f.add(exit);
+
+        JButton login = new JButton("Back to Login");
+        login.setForeground(Color.white);
+        login.setBackground(new Color(255, 128, 0));
+        login.setBounds(340, 380, 130, 50);
+        f.add(login);
+        exit.addActionListener(e -> System.exit(0));
+
+        displaybtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                f.dispose();
+                new Display();
+            }
+        });
+
+        JPanel panel2 = new JPanel();
+        panel2.setBounds(0, 0, 800, 600);
         panel2.setLayout(new BorderLayout());
         f.add(panel2);
 
-          try {
+        try {
             BufferedImage originalImage = ImageIO.read(getClass().getResource("./BackgroungImg.png"));
             int scaledWidth = 800;
             int scaledHeight = 600;
@@ -74,7 +86,7 @@ public class BookStore implements ActionListener {
             g.drawImage(originalImage, 0, 0, scaledWidth, scaledHeight, null);
             g.dispose();
             imageIcon = new ImageIcon(scaledImage);
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -82,24 +94,28 @@ public class BookStore implements ActionListener {
         imageLabel.setBounds(400, 50, 200, 300);
         panel2.add(imageLabel);
 
-        
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         login.addActionListener(new ActionListener() {
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            f.dispose();
-            new Login();
-          }
-          });
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                f.dispose();
+                new Login();
+            }
+        });
     }
 
-    
     public void actionPerformed(ActionEvent e) {
-       f.dispose();
+        f.dispose();
         new AddBook();
     }
+
     public static void main(String[] args) {
-        new BookStore();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new BookStore();
+            }
+        });
     }
 }
---
+++ Display.java
@@ -0,0 +1,99 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Display {
+    JFrame f;
+    JTable j;
+
+    Display() {
+        f = new JFrame("Display Records");
+        f.setSize(400, 300);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLocationRelativeTo(null);
+        f.setUndecorated(true);
+        f.getRootPane().setBorder(BorderFactory.createLineBorder(new Color(102, 0, 51), 3));
+
+        JLabel heading = new JLabel("Book Store");
+        heading.setFont(new Font("Serif", Font.BOLD, 32));
+        heading.setForeground(new Color(102, 0, 51));
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+
+        String[] columnNames = { "BOOK NAME", "BOOK ID", "PRICE", "AUTHOR NAME" };
+        DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/library";
+            String user = "root";
+            String password = "1234";
+            Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            String query = "SELECT Bname, Bid, Price, Author FROM books";
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                String name = resultSet.getString("Bname");
+                String id = resultSet.getString("Bid");
+                String price = resultSet.getString("Price");
+                String author = resultSet.getString("Author");
+                model.addRow(new Object[] { name, id, price, author });
+            }
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        j = new JTable(model);
+        j.setPreferredScrollableViewportSize(new Dimension(300, 100));
+        j.setFillsViewportHeight(true);
+
+        TableColumnModel columnModel = j.getColumnModel();
+        columnModel.getColumn(0).setPreferredWidth(50);
+        columnModel.getColumn(1).setPreferredWidth(1); 
+        columnModel.getColumn(2).setPreferredWidth(1);
+        columnModel.getColumn(3).setPreferredWidth(140);
+
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+
+        for (int i = 0; i < j.getColumnCount(); i++) {
+            j.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
+        }
+
+        JScrollPane sp = new JScrollPane(j);
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setBackground(new Color(102, 0, 51));
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.setForeground(Color.WHITE);
+        exitButton.setBackground(Color.RED);
+        exitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        bottomPanel.add(exitButton);
+
+        f.add(heading, BorderLayout.NORTH);
+        f.add(sp, BorderLayout.CENTER);
+        f.add(bottomPanel, BorderLayout.SOUTH);
+
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Display();
+            }
+        });
+    }
+}
